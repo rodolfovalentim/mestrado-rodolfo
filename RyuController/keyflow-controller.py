6da95313c6d99a82d3ed799fc62f95b9e2634750
rodolfovalentim <rodolfovieiravalentim@gmail.com>
2019-10-30 21:46:42 -0300

    iniciando keyflow controller

--- RyuController/keyflow-controller.py
+++ RyuController/keyflow-controller.py
@@ -11,8 +11,8 @@ class KeyflowController(app_manager.RyuApp):
 
     def __init__(self, *args, **kwargs):
         super(KeyflowController, self).__init__(*args, **kwargs)
-        self.hw_addr = '0a:e4:1c:d1:3e:44'
-        self.ip_addr = '192.0.2.9'
+        self.ip_to_ether = {
+            "10.0.0.1": "2a:54:59:60:bb:15", "10.0.0.2": "3a:3f:43:e2:b0:38"}
 
     # This method add a flow to send every packet to controller
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -22,17 +22,11 @@ class KeyflowController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        self.logger.info("Switch added. DPID: %s" % (datapath.id,))
-
-        actions = [parser.OFPActionOutput(port=ofproto.OFPP_CONTROLLER,
-                                          max_len=ofproto.OFPCML_NO_BUFFER)]
-        inst = [parser.OFPInstructionActions(type_=ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions=actions)]
-        mod = parser.OFPFlowMod(datapath=datapath,
-                                priority=0,
-                                match=parser.OFPMatch(),
-                                instructions=inst)
-        datapath.send_msg(mod)
+        # install the table-miss flow entry.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
 
     # It need a method to receive a packet in using of keyflow and add a flow to process
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -41,19 +35,18 @@ class KeyflowController(app_manager.RyuApp):
         datapath = msg.datapath
         port = msg.match['in_port']
         pkt = packet.Packet(data=msg.data)
-        self.logger.info("packet-in %s" % (pkt,))
-        # pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
-        # if not pkt_ethernet:
-        #     return
-        # pkt_arp = pkt.get_protocol(arp.arp)
-        # if pkt_arp:
-        #     self._handle_arp(datapath, port, pkt_ethernet, pkt_arp)
-        #     return
-        # pkt_ipv4 = pkt.get_protocol(ipv4.ipv4)
-        # pkt_icmp = pkt.get_protocol(icmp.icmp)
-        # if pkt_icmp:
-        #     self._handle_icmp(datapath, port, pkt_ethernet, pkt_ipv4, pkt_icmp)
-        #     return
+        # self.logger.info("packet-in %s" % (pkt,))
+        pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
+        if not pkt_ethernet:
+            return
+        pkt_arp = pkt.get_protocol(arp.arp)
+        if pkt_arp:
+            self._handle_arp(datapath, port, pkt_ethernet, pkt_arp)
+            return
+        pkt_keyflow = pkt.get_protocol(ethernet.ethernet)
+        if pkt_keyflow:
+            self._handle_keyflow(datapath, port, pkt_keyflow, pkt)
+            return
 
     def _handle_arp(self, datapath, port, pkt_ethernet, pkt_arp):
         if pkt_arp.opcode != arp.ARP_REQUEST:
@@ -61,29 +54,48 @@ class KeyflowController(app_manager.RyuApp):
         pkt = packet.Packet()
         pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
                                            dst=pkt_ethernet.src,
-                                           src=self.hw_addr))
+                                           src=self.ip_to_ether[pkt_arp.dst_ip]))
         pkt.add_protocol(arp.arp(opcode=arp.ARP_REPLY,
-                                 src_mac=self.hw_addr,
-                                 src_ip=self.ip_addr,
+                                 src_mac=self.ip_to_ether[pkt_arp.dst_ip],
+                                 src_ip=pkt_arp.dst_ip,
                                  dst_mac=pkt_arp.src_mac,
                                  dst_ip=pkt_arp.src_ip))
+        self.logger.info("sending arp %s to %s" % (pkt, port, ))
         self._send_packet(datapath, port, pkt)
 
-    def _handle_icmp(self, datapath, port, pkt_ethernet, pkt_ipv4, pkt_icmp):
-        if pkt_icmp.type != icmp.ICMP_ECHO_REQUEST:
-            return
-        pkt = packet.Packet()
-        pkt.add_protocol(ethernet.ethernet(ethertype=pkt_ethernet.ethertype,
-                                           dst=pkt_ethernet.src,
-                                           src=self.hw_addr))
-        pkt.add_protocol(ipv4.ipv4(dst=pkt_ipv4.src,
-                                   src=self.ip_addr,
-                                   proto=pkt_ipv4.proto))
-        pkt.add_protocol(icmp.icmp(type_=icmp.ICMP_ECHO_REPLY,
-                                   code=icmp.ICMP_ECHO_REPLY_CODE,
-                                   csum=0,
-                                   data=pkt_icmp.data))
-        self._send_packet(datapath, port, pkt)
+    def _handle_keyflow(self, datapath, port, pkt_keyflow, pkt):
+        # self.logger.info("packet-in %s" % (pkt,))
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install the table-miss flow entry.
+        match = parser.OFPMatch(in_port=port, eth_dst='90:00:00:00:00:00')
+        actions = [parser.OFPActionOutput(2 % 2, ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        return
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+        return
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
 
     def _send_packet(self, datapath, port, pkt):
         ofproto = datapath.ofproto
